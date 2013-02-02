82501c8b860ba9fc517787064e1eeca88d479097
Diego Didona <didona@gsd.inesc-id.pt>
2013-02-02 15:32:36 +0000

    * Adding custom config files

--- build.sh
+++ build.sh
@@ -22,6 +22,8 @@ echo "Compiling WPM"
 cd ${WPM_HOME}
 ant
 cd ..
+echo "Copying custom scripts and configuration files for wpm"
+cp -r custom/* ${WPM_HOME}
 echo "Downloading TasController package"
 git clone https://github.com/didona/test.git ${TASC_HOME}
 echo "Downloading the WPMConnector package"
@@ -29,7 +31,6 @@ git clone https://github.com/cloudtm/Workload_Monitor_Connector ${WPMC_HOME}
 echo "Compiling the WPMConnector package"
 cd ${WPMC_HOME}
 ant
-echo "&" >> run_log_service.sh
 cd ..
 echo "Moving WPMConnector jar to Tas Controller's lib folder"
 cp ${WPMC_HOME}/${WPMC_ARTIFACT} ${TASC_HOME}/lib/
